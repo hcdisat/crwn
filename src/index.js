e9dc694a624c6096ef4279b823c25171546ba739
Hector Caraballo <hcdisat@gmail.com>
2019-09-29 10:21:24 -0400

    fixing google login

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
-import "./index.css";
 import React from "react";
 import ReactDOM from "react-dom";
+
 import { Provider as ReduxProvider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 import { PersistGate } from "redux-persist/integration/react";
-import App from "./App";
+
 import { store, persistor } from "./redux/store";
+import App from "./App";
+import "./index.css";
 
 ReactDOM.render(
   <ReduxProvider store={store}>
